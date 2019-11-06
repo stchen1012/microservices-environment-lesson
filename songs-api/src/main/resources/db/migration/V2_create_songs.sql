ddac52b1377152c1451ae0d8adea25debf1ba768
Tracy Chen <stchen1012@gmail.com>
2019-11-05 23:05:07 -0500

    added sql file and afterMigrate to songs api but DB still not connecting

---
+++ songs-api/src/main/resources/db/migration/V2_create_songs.sql
@@ -0,0 +1,5 @@
+CREATE TABLE songs (
+    id SERIAL,
+    song_title VARCHAR(100) NOT NULL,
+    song_length INTEGER(10)
+);
---
+++ songs-api/src/main/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,3 @@
+INSERT INTO songs(song_title, song_length)
+VALUES ('batman', 1.5),
+('robin', 2.5);
